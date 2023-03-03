6218b759607023eb95513ecaed582c02f2949ab7
azdw <112703311+azdw@users.noreply.github.com>
2023-03-03 21:10:31 +0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,29 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+
+
+    function auth(req, res, next) {
+        // Check for a JWT in the Authorization header
+        const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+        if (!token) {
+            // No token provided, so send a 401 Unauthorized response
+            return res.status(401).json({ message: 'Authorization token missing' });
+        }
+
+        try {
+            // Verify the JWT using a secret key
+            const decoded = jwt.verify(token, 'my-secret-key');
+            // Store the decoded user information on the request object for later use
+            req.user = decoded;
+            // Call the next middleware function
+            next();
+        } catch (err) {
+            // JWT verification failed, so send a 401 Unauthorized response
+            return res.status(401).json({ message: 'Invalid authorization token' });
+        }
+        }
+
 });
  
 const PORT =5000;
